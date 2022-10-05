33d662361d61c285d98e46bc66a20033a26165c9
Alexsandro <alexsandrojunior53@gmail.com>
2022-10-05 13:27:43 -0300

    arquivos adicionados

---
+++ src/js/validation.js
@@ -0,0 +1,18 @@
+const btnSubmit = document.getElementById("btn");
+const inputs = document.querySelectorAll(".inputs");
+const errorAlert = document.querySelectorAll(".error-alert");
+
+btnSubmit.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    inputs.forEach((item, index) => {
+
+        if (item.value === "") {
+            inputs[index].classList.add("input-invalid");
+            errorAlert[index].style.display= 'block'
+        } else {
+            inputs[index].classList.add("input-valid");
+        }
+
+    });
+})
