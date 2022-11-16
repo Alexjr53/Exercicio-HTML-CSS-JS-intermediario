61fb66f722a63d766a9611b67ce70c3d641ad320
Alexsandro <alexsandrojunior53@gmail.com>
2022-11-16 01:18:26 -0300

    código refatorado

--- src/js/validation.js
+++ src/js/validation.js
@@ -4,15 +4,15 @@ const errorAlert = document.querySelectorAll(".error-alert");
 
 btnSubmit.addEventListener("click", function (event) {
     event.preventDefault();
-    
-    inputs.forEach((item, index) => {
 
+     inputs.forEach((item, index) => {
         if (item.value === "") {
             inputs[index].classList.add("input-invalid");
             errorAlert[index].style.display= 'block'
-        } else {
+         } else {
+            inputs[index].classList.remove("input-invalid");
             inputs[index].classList.add("input-valid");
-        }
-
-    });
+            errorAlert[index].style.display= 'none'
+         }
+        });
 })
